47b9e6fbd27288c45f8373015e166585693299b2
Joshua Taguicana <joshtaguicana@gmail.com>
2020-02-17 20:53:22 -0500

    added about in hero

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,14 +1,64 @@
-import React, { Component } from "react";
-import "./Hero.scss";
+import React, { Component } from "react"
+import "./Hero.scss"
 import styled from "styled-components"
 import Title from "./Title"
-import Plx from "react-plx";
+import Plx from "react-plx"
 
 const HeroContainer = styled.section`
-  background: radial-gradient(ellipse at bottom, #002f69 0%, #02275c 30%, #000000 100%);
+  position: relative;
+  background: radial-gradient(
+    ellipse at bottom,
+    #002f69 0%,
+    #02275c 30%,
+    #000000 100%
+  );
   overflow: hidden;
-  height: 120vh;
+  height: 160vh;
+
 `
+
+const AboutContainer = styled.article`
+  position: relative;
+  width: 16rem;
+  margin: 0 auto;
+  bottom: -80vh;
+  @media (min-width: 48rem) {
+    width: 34rem;
+  }
+  @media (min-width: 64rem) {
+    width: 50rem;
+    font-size: 3.5rem;
+  }
+`
+
+const AboutTitle = styled.h2`
+  color: #fff;
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom: 1rem;
+  @media (min-width: 48rem) {
+    font-size: 2.5rem;
+  }
+  @media (min-width: 64rem) {
+    font-size: 3.5rem;
+  }
+`
+
+const AboutDescription = styled.p`
+  color: #fff;
+  text-align: center;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  @media (min-width: 48rem) {
+    font-size: 1.5rem;
+    line-height: 2rem;
+  }
+  @media (min-width: 64rem) {
+    font-size: 2rem;
+    line-height: 3rem;
+  }
+`
+
 const parallax = [
   {
     start: "self",
@@ -18,26 +68,52 @@ const parallax = [
     properties: [
       {
         startValue: 0,
-        endValue: -400,
+        endValue: -800,
         property: "translateY",
-      }
-    ]
+      },
+    ],
+  },
+]
+
+const aboutPlx = [
+  {
+    start: "self",
+    end: "self",
+    endOffset: "160vh",
+    properties: [
+      {
+        startValue: 0,
+        endValue: -2400,
+        property: "translateY",
+      },
+    ],
   }
-];
+]
 
 class Hero extends Component {
   render() {
     return (
-      <HeroContainer >
+      <HeroContainer>
         <div id="stars" />
         <div id="stars2" />
         <div id="stars3" />
         <Plx parallaxData={parallax}>
           <Title />
         </Plx>
+        <Plx parallaxData={aboutPlx}>
+          <AboutContainer>
+            <AboutTitle>About</AboutTitle>
+            <AboutDescription>
+              I'm a new web developer dedicated to building creative websites
+              and continuing to learn new skills in web development. Currently
+              working as a TA at BrainStation, but looking for new
+              opportunities! Check out some of my projects below!
+            </AboutDescription>
+          </AboutContainer>
+        </Plx>
       </HeroContainer>
     )
   }
 }
 
-export default Hero;
+export default Hero
